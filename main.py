6e973a101b1a6d3dcd03874750c960f6064ea386
sujinee01 <kmosujin@gmail.com>
2024-04-01 16:52:43 +0900

    M

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Hello world!")
+name ='sujin'
+print(f'I\'m SJ LEE!')
+programming_language='PYthon'
+print('Most used language is {0}'.forgmat(programming_language))
--- prac5/main.py
+++ prac5/main.py
@@ -1,5 +1,2 @@
+print("Hello Sujin Lee!")
 print("Hello world!")
-name ='sujin'
-print(f'I\'m SJ LEE!')
-programming_language='PYthon'
-print('Most used language is {0}'.forgmat(programming_language))
