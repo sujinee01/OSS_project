525eda0c2b5e710b0e633c28e66076004c9bfbd8
sujinee01 <kmosujin@gmail.com>
2024-04-07 17:20:26 +0900

    main.py: add grade

--- Psub5/main.py
+++ Psub5/main.py
@@ -1,8 +1,10 @@
 name = input ("이름을 입력하세요: ")
 studentnum = input ("학번을 입력하세요: ")
 department = input ("학과를 입력하세요: ")
+grade = input("학년을 입력하세요: ")
 
 print ("<출력>")
 print("이름: " + name)
 print("학번: " + studentnum)
 print("학과: "+ department)
+print("학년: "+grade)
