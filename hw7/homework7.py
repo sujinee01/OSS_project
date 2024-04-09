a36ac9f0395cc5757f05609cac420fe875eede64
sujinee01 <kmosujin@gmail.com>
2024-04-09 12:45:06 +0900

    Add homework7.py

---
+++ hw7/homework7.py
@@ -0,0 +1,12 @@
+def prime_factors(n):
+    factors = []
+divisor = 2
+    while n > 1:
+        while n % divisor == 0:
+            factors.append(divisor)
+            n /= divisor
+        divisor -= 1
+    return factor
+
+number = float(input("소인수분해할 수를 입력하세요: "))
+print(f"{number}의 소인수분해 결과:", prime_factors(num))
